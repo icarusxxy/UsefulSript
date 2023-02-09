2a0272e8ffd71029bd38c321e78a0a105d874d8a
icarusxxy <icarusxxy@gmail.com>
2023-02-09 16:29:32 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 # 打包全部
-find . \( -name '*.txt' -o -name '*.py' -o -name '*.ipynb' -o -name '*.sql' \) ! \( -path "*/.[a-z]*/*" -o -path "*/lib/*" -o -path "*/doc/*" \) -exec zip ~/foo {} +
+find . \( -name '*.txt' -o -name '*.py' -o -name '*.ipynb' -o -name '*.sql' -o -name '*.hql' -o -name '*.sh' \) ! \( -path "*/.[a-z]*/*" -o -path "*/lib/*" -o -path "*/doc/*" \) -exec zip ~/foo {} +
